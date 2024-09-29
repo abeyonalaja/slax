ccca9f438570a529db5b219790bf730b3e59b170
abey onalaja <abey.onalaja@gmail.com>
2024-09-29 13:27:02 +0100

    show timestamps in correct timezone

--- lib/slax_web/components/ui/message_item.ex
+++ lib/slax_web/components/ui/message_item.ex
@@ -2,8 +2,10 @@ defmodule SlaxWeb.MessageItem do
   use SlaxWeb, :live_component
   alias SlaxWeb.ChatRoomLive
 
-  defp message_timestamp(message) do
+  defp message_timestamp(message, timezone) do
     message.inserted_at
+    |> Timex.Timezone.convert(Timex.Timezone.local())
+    |> Timex.Timezone.convert(timezone)
     |> Timex.format!("%-l:%M %p", :strftime)
   end
 end
