58ba991c8e53768b9b0c10a91aa3962a38296e18
abey onalaja <abey.onalaja@gmail.com>
2024-09-29 13:08:24 +0100

    add timestamps formatted with Timex

--- lib/slax_web/components/ui/message_item.ex
+++ lib/slax_web/components/ui/message_item.ex
@@ -1,4 +1,9 @@
 defmodule SlaxWeb.MessageItem do
   use SlaxWeb, :live_component
   alias SlaxWeb.ChatRoomLive
+
+  defp message_timestamp(message) do
+    message.inserted_at
+    |> Timex.format!("%-l:%M %p", :strftime)
+  end
 end
